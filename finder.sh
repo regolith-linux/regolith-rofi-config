9b3d40cafb7044f3a1741e205e0071889e8a9ba0
Ken Gilmer <kgilmer@gmail.com>
2020-04-11 11:29:13 -0700

    Improvements to file search dialog.

--- finder.sh
+++ finder.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
+# This script has been adapted from https://github.com/davatorium/rofi-scripts/blob/master/rofi-finder/finder.sh
 
 ITEM_LIMIT=$(xrescat rofi.search.limit 64)
 SHOW_HELP=$(xrescat rofi.search.help "true")
 
-# This script has been adapted from https://github.com/davatorium/rofi-scripts/blob/master/rofi-finder/finder.sh
-if [ ! -z "$@" ]
-then
+print_help() {
+    echo "Returns matches from all local files"
+    echo "Type search term, hit enter"
+    echo "Type more for further filtering"
+    echo "or scroll down"
+    echo "Hit <enter> on a line to launch"
+    echo "To search again type: !<search_query>"
+    echo "To scroll horizontally type: <alt> ."
+}
+
+if [ ! -z "$@" ]; then
+  # A search parameter was passed from the dialog
   QUERY=$@
-  if [[ "$@" == /* ]]
-  then
-    if [[ "$@" == *\?\? ]]
-    then
+  if [[ "$@" == /* ]]; then
+    # A search item was selected, try to launch it
+    if [[ "$@" == *\?\? ]]; then
       coproc ( xdg-open "${QUERY%\/* \?\?}"  > /dev/null 2>&1 )
       exec 1>&-
       exit;
@@ -19,28 +28,30 @@ then
       exec 1>&-
       exit;
     fi
-  elif [[ "$@" == \!\!* ]]
-  then
-    echo "This dialog will return matches from all local files."
-    echo "Type a search term, hit enter. Results are returned."
-    echo "Type more for further filtering or scroll down."
-    echo "Hit <enter> on a line to launch with default app."
-    echo "To search again type: !<search_query>"
-    echo "To scroll horizontally type: <alt> ."
-  elif [[ "$@" == \?* ]]
-  then
+  elif [[ "$@" == \!\!* ]]; then
+    # Help was requested, print it.
+    print_help
+  elif [[ "$@" == \?* ]]; then
+    # Filter existing results
     while read -r line; do
       echo "$line" \?\?
     done <<< $(locate --limit $ITEM_LIMIT "$QUERY" 2>&1 | grep -v 'Permission denied\|Input/output error')
   else
-    locate --limit $ITEM_LIMIT "$QUERY" 2>&1 | grep -v 'Permission denied\|Input/output error'
+    if [[ $QUERY = "~/"* ]]; then 
+      # Manually expand ~ to $HOME, based on https://unix.stackexchange.com/a/399439      
+      QUERY="${HOME}/${QUERY#"~/"}"
+    fi
+    if [[ -f "$QUERY" ]]; then
+      # User entered a complete file path, so just launch it
+      coproc ( xdg-open "$QUERY"  > /dev/null 2>&1 )
+      exec 1>&-
+      exit;
+    else
+      # Search for query
+      locate --limit $ITEM_LIMIT $QUERY 2>&1 | grep -v 'Permission denied\|Input/output error'
+    fi
   fi
-elif [[ "$SHOW_HELP" == "true" ]]
-then
-  echo "This dialog will return matches from all local files."
-  echo "Type a search term, hit enter. Results are returned."
-  echo "Type more for further filtering or scroll down."
-  echo "Hit <enter> on a line to launch with default app."
-  echo "To search again type: !<search_query>"
-  echo "To scroll horizontally type: <alt> ."
+elif [[ "$SHOW_HELP" == "true" ]]; then
+  #Initial execution, print help
+  echo "File search, enter !! for help"
 fi
